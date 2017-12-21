e23dbf2f79be4be4bd69791cd377804308aa91f8
carlosazaustre <cazaustre@gmail.com>
2017-12-21 17:06:36 +0100

    add main scaffolding

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,6 @@ body {
   margin: 0;
 }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
 main {
   text-align: center;
   margin-top: 40px;
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <btn-add></btn-add>
+  </div>
 </template>
 
 <script>
+import BtnAdd from '@/components/BtnAdd'
+
 export default {
   name: 'home',
+  components: { BtnAdd, BtnLogin },
+
   data () {
     return {
       title: 'Home'
