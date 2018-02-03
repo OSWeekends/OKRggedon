fb828198c3fca553934782e57a6a2bf8a38c4107
Carlos Azaustre <cazaustre@gmail.com>
2018-02-03 23:25:10 +0100

    restart from zero to hero

--- src/api/settings.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAlW8JXBsAwUKxIvtmmLw9xEQsdi1LYoHM',
-  authDomain: 'okrggedon.firebaseapp.com',
-  databaseURL: 'https://okrggedon.firebaseio.com',
-  projectId: 'okrggedon',
-  storageBucket: 'okrggedon.appspot.com',
-  messagingSenderId: 'messagingSenderId'
-}
